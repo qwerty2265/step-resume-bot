a7b77e96a27759c35230094273e25d48e3c3c0f4
Maks <maks_mun@bk.ru>
2023-07-19 00:33:55 +0600

    feat(backend): Add start message keyboard

--- src/commands/start_command.py
+++ src/commands/start_command.py
@@ -1,4 +1,15 @@
 from aiogram import types
 
 async def start_command(msg: types.Message) -> None:
-    await msg.answer('Привет, студент!) Я создан для того, чтобы помогать в составление резюме. Я помогу тебе сделать первый шаг в увлекательный мир профессионального развития и карьерного продвижения. ')
+    message: str = 'Привет, студент!) Я создан для того, чтобы помогать в составление резюме. Я помогу тебе сделать первый шаг в увлекательный мир профессионального развития и карьерного продвижения.'
+
+    keyboard_button1 = types.KeyboardButton('Хочу составить резюме')
+    keyboard_button2 = types.KeyboardButton('Контакты КЦ')
+    keyboard_button3 = types.KeyboardButton('Вернуться в начало')
+
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    keyboard.add(keyboard_button1, keyboard_button2)
+    keyboard.add(keyboard_button3)
+
+    await msg.answer(message, reply_markup=keyboard)
