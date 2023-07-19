fd81415675d7a96be1cd850f10ad8c88ad9812d8
Maks <maks_mun@bk.ru>
2023-07-20 01:00:09 +0600

    feat(backend): Create help command

--- src/commands/bot_commands.py
+++ src/commands/bot_commands.py
@@ -3,6 +3,7 @@ from aiogram.dispatcher.filters import Text
 from .start_command import start_command
 from .callcenter_command import callcenter_command
 from .resume_command import resume_command
+from .help_command import help_command
 from ..state import *
 
 def register_commands(dp):
@@ -10,6 +11,8 @@ def register_commands(dp):
     dp.register_message_handler(
         start_command, Text(equals="вернуться в начало", ignore_case=True), state="*"
     )
+
+    dp.register_message_handler(help_command, commands=['help'], state="*")
 
     dp.register_message_handler(callcenter_command, commands=['callcenters'], state="*")
     dp.register_message_handler(
---
+++ src/commands/help_command.py
@@ -0,0 +1,20 @@
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+
+async def help_command(msg: types.Message, state: FSMContext) -> None:
+    message: str = '''
+Привет, студент!) Я создан для того, чтобы помогать в составление резюме.
+Я помогу тебе сделать первый шаг в увлекательный мир профессионального развития и карьерного продвижения.
+
+Для этого у меня есть:
+
+/resume - команда, для начала составления резюме со мной.
+/callcenters - команда, показывающая список доступных колл-центров
+'''
+
+    keyboard_button = types.KeyboardButton(text='Вернуться в начало')
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(keyboard_button)
+
+    await state.finish()
+    await msg.answer(message, reply_markup=keyboard)
