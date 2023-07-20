4596b42a04ea22435c70d3520ff0b2da40e0d8e2
Maks <maks_mun@bk.ru>
2023-07-20 06:07:07 +0600

    feat(backend): Create encryption and descryption functions

---
+++ database/encrypt_decrypt_.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from cryptography.fernet import Fernet
+
+# Функция для шифрования данных
+def encrypt_data(data):
+    cipher_suite = Fernet(os.getenv("ENCRYPTION_KEY"))
+    encrypted_data = cipher_suite.encrypt(data.encode())
+    return encrypted_data
+
+# Функция для дешифрования данных
+def decrypt_data(encrypted_data):
+    cipher_suite = Fernet(os.getenv("ENCRYPTION_KEY"))
+    decrypted_data = cipher_suite.decrypt(encrypted_data).decode()
+    return decrypted_data
